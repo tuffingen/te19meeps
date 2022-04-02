8fa8bae6140192b4469d68ad0cbf6b9f5b67f2d1
tuffingen <isac.danielsson03@gmail.com>
2022-04-02 18:35:49 +0200

    a

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/meeps', meepsRouter);
 
+
 nunjucks.configure('views', {
   autoescape: true,
   express: app
